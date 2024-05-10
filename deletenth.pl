73217456d3d29e67f9af67724c64691d7183f749
knightn <120507925+knightn@users.noreply.github.com>
2024-05-10 12:03:03 +0530

    deletenth.pl

--- deletenth.pl
+++ deletenth.pl
@@ -1,3 +1,5 @@
 delete_nth(1, [_|L], L) :- !.
 delete_nth(P, [H|L], [H|R]) :- P1 is P - 1,
                                delete_nth(P1, L, R).
+
+                               delete_nth(1, [1, 2, 3, 4], L).
