fa284c9c2d3a8ebb4e818e0933aa56c7a3f9d224
knightn <120507925+knightn@users.noreply.github.com>
2024-05-10 11:51:14 +0530

    power.pl

--- power.pl
+++ power.pl
@@ -4,3 +4,5 @@ power(Num, Power, Ans) :- Num > 0, Power > 0,
                           P1 is Power - 1,
                           power(Num, P1, A1),
                           Ans is A1 * Num.
+
+                          power(2, 8, A).
