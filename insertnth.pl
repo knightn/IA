60210288dbefe4f3859bf97fa49b4d21a2b5a1cd
knightn <120507925+knightn@users.noreply.github.com>
2024-05-10 12:03:44 +0530

    insertnth.pl

--- insertnth.pl
+++ insertnth.pl
@@ -1,3 +1,5 @@
 insert_nth(E, 1, L, [E|L]) :- !.
 insert_nth(E, P, [H|L], [H|R]) :- P1 is P - 1,
                                   insert_nth(E, P1, L, R).
+
+                                  insert_nth(5, 2, [1, 2, 3, 4], L).
