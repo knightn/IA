594905d6a0087f58423c79eeff4260145a4b2973
knightn <120507925+knightn@users.noreply.github.com>
2024-05-10 11:46:50 +0530

    fibonnaci.pl

--- generate_fib.pl
+++ generate_fib.pl
@@ -6,3 +6,5 @@ generate_fib(N, T) :- N1 is N - 1,
                       N2 is N - 2,
                       generate_fib(N2, T2),
                       T is T1 + T2. 
+
+                      generate_fib(7, T).
